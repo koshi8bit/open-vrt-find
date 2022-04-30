a2ee5248c0c5e71f68cc6457c4c3ee38522c7af5
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2022-05-01 00:36:03 +0700

    refactor

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pandas as pd
 
-if __name__ == '__main__':
+
+def read_csv() -> pd.DataFrame:
     file_path = "ToH_dump_tab_separated.csv"
-    file_path = file_path
     df2 = pd.read_csv(file_path, sep="\t", index_col=0)
     # Pandas.print(df)
     df = df2[["brand", "model"]]
     df["full"] = df["brand"] + " " + df["model"]
+
+    return df
+
+
+def main():
+    df = read_csv()
     # Pandas.print(df)
     print(list(df["full"]))
 
+
+if __name__ == '__main__':
+    main()
